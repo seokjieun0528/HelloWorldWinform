aa2d436d040a964a042127a19781d129c616bd4d
Another Name <s2307@e-mirim.hs.kr>
2025-04-15 16:11:25 +0900

    모달과 모달리스 창

---
+++ FormMain.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorldWinform
+{
+    public partial class FormMain: Form     
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPush_Click(object sender, EventArgs e)
+        {
+            lblTitle.Text = "버튼클릭";
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 끝내기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void 프로그램정보ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form formAbout1 = new FormAbout();
+            Form formAbout2 = new FormAbout();
+
+            // 모달(Modal) 창
+            formAbout1.Text = "모달창";
+            formAbout1.ShowDialog();
+
+            // 모달리스(Modeless) 창
+            formAbout1.Text = "모달리스창";
+            formAbout2.Show();
+        }
+    }
+}
